e5e445cac1aa57eebdc1b5669f10ff2c93c91b89
rickyhehe10 <dev.rickymuli@gmail.com>
2023-05-10 10:58:22 +0700

    add

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const services = require('./services')
+const dayjs = require('dayjs')
 
 dotenv.config()
 const app = express();
 app.use(morgan('[:method :url :status] :response-time ms [:date[clf]] ":user-agent" '))
+
+app.get('/', async (req, res) => {
+  return res.json({ date: dayjs() })
+})
 
 app.get('/get-bca', async (req, res) => {
   const response = await services.getTrxBCA(req.query.username, req.query.password)
