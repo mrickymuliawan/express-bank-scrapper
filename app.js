f4f9061bca0b8fa561d40919be58bd832b671688
rickyhehe10 <dev.rickymuli@gmail.com>
2024-02-12 16:57:34 +0700

    update

--- app.js
+++ app.js
@@ -3,21 +3,64 @@ const dotenv = require('dotenv')
 const morgan = require('morgan')
 const services = require('./services')
 const dayjs = require('dayjs')
+const puppeteer = require('puppeteer-extra')
+const { Cluster } = require('puppeteer-cluster');
 
 dotenv.config()
 const app = express();
 app.use(morgan('[:method :url :status] :response-time ms [:date[clf]] ":user-agent" '))
+app.use(express.json());
 
-app.get('/', async (req, res) => {
-  return res.json({ date: dayjs() })
-})
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin());
 
-app.get('/get-bca', async (req, res) => {
-  const response = await services.getTrxBCA(req.query.username, req.query.password)
-  console.log(response);
-  return res.json({ response })
-})
+(async () => {
 
-app.listen(process.env.PORT, () => {
-  console.log(`server listen on port ${process.env.PORT}`)
-})
+  const cluster = await Cluster.launch({
+    concurrency: Cluster.CONCURRENCY_CONTEXT,
+    maxConcurrency: 3,
+    puppeteerOptions: {
+      headless: true,
+      executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+      args: [
+        '--log-level=3', // fatal only
+        '--no-default-browser-check',
+        '--no-sandbox',
+        '--no-first-run',
+        '--no-zygote',
+        // '--disable-infobars',
+        // '--disable-web-security',
+        // '--disable-site-isolation-trials',
+      ],
+    }
+  });
+
+  app.get('/', async (req, res) => {
+    return res.json({ date: dayjs() })
+  })
+
+  app.post('/get-bca', async (req, res) => {
+    const response = await services.getTrxBCA(req.body.username, req.body.password)
+    return res.json({ response })
+  })
+
+
+  app.post('/pay-razer', async (req, res) => {
+    const response = await services.razerCheckoutPage(cluster, req.body.data, req.body.cookies, req.body.otpSecret)
+    return res.json({ response })
+  })
+
+  app.get('/test-scrap', async (req, res) => {
+    const response = services.testScraper(cluster, req.query.error)
+    return res.json({ response })
+  })
+
+  app.post('/test-bca', async (req, res) => {
+    const response = services.getMutationBCA(req.body.username, req.body.password)
+    return res.json({ response })
+  })
+
+  app.listen(process.env.PORT, () => {
+    console.log(`server listen on port ${process.env.PORT}`)
+  })
+})()
--- services.js
+++ services.js
@@ -2,28 +2,175 @@
 const bankScraper = require('@rickyhehe10/bank-scraper')
 const cryptojs = require('crypto-js')
 const dayjs = require('dayjs')
+const puppeteer = require('puppeteer')
+const otpauth = require('otpauth')
+const axios = require('axios')
+const dayjs2 = require('dayjs')
+
+function getAuthenticatorOTP(secret) {
+  let totp = new otpauth.TOTP({
+    issuer: "ACME",
+    label: "AzureDiamond",
+    algorithm: "SHA1",
+    digits: 6,
+    period: 30,
+    secret: secret,
+  });
+  let token = totp.generate();
+  return token
+}
+
+async function testScraper(cluster, error) {
+  // cluster.queue(async ({ page }) => {
+
+  try {
+
+    await page.goto('https://www.google.com', { waitUntil: 'networkidle0' });
+    await sleep(1000)
+
+    if (error) {
+      throw new Error('awkaka')
+    }
+    await sleep(3000)
+    await page.screenshot({ path: 'test.png' });
+    console.log('done');
+    return true
+  } catch (error) {
+    console.log('error');
+  }
+  finally {
+    await page.close()
+  }
+  // });
+
+}
+
+async function razerCheckoutPage(cluster, data, cookies, otpSecret) {
+  cluster.queue(async ({ page }) => {
+
+    const setCookie = [{
+      name: '_rzru',
+      value: cookies['_rzru'],
+      domain: '.oauth2.razer.com'
+    }, {
+      name: '_rzrsess',
+      value: cookies['_rzrsess'],
+      domain: '.oauth2.razer.com'
+    }];
+
+
+    const startCounting = dayjs().unix()
+
+    try {
+      await page.setRequestInterception(true);
+
+      page.on('request', (req) => {
+        if (req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image') {
+          req.abort();
+        }
+        else {
+          req.continue();
+        }
+      });
+
+      await page.setCookie(...setCookie);
+      console.log('RAZER PAYMENT - START ' + data.orderId);
+
+      await page.goto(data.paymentUrl, { waitUntil: 'networkidle2' });
+
+      // await page.click('#onetrust-accept-btn-handler')
+
+      // await page.waitForSelector('#loginEmail');
+      // await page.type('#loginEmail', username);
+
+      // await page.waitForSelector('#loginPassword');
+      // await page.type('#loginPassword', password);
+
+      // await page.focus('#loginPassword')
+      // await page.keyboard.press('Enter');
+      // await page.waitForSelector('#dialogPanel1 > div.btn_container > a');
+      // await page.click('#dialogPanel1 > div.btn_container > a');
+
+      // * if link expired
+      let symbolError = (await page.$('div.symbol')) || false;
+      if (symbolError) {
+        console.log('RAZER PAYMENT - EXPIRED ' + data.orderId);
+      }
+
+      await page.waitForSelector('#btn99', {
+        visible: true,
+      });
+
+      await page.$eval('#btn99', (elem) => elem.click());
+
+      // await page.waitForNavigation({ waitUntil: "networkidle0" })
+      await page.waitForSelector('#showOTPIFrame', { visible: true });
+      const elementHandle = await page.$(
+        'iframe#razerOTP',
+      );
+      const frame = await elementHandle.contentFrame();
+      await frame.waitForSelector('#modal-one-time-password-1');
+
+
+      const otpInput = '.input-otp'
+
+      await frame.waitForSelector(otpInput);
+
+      await frame.$eval(otpInput, (elem) => elem.focus());
+      const authOtp = getAuthenticatorOTP(otpSecret)
+      await page.keyboard.type(authOtp)
+
+      // wait until order complete page
+      await page.waitForSelector('.symbol');
+
+      console.log('RAZER PAYMENT - SUCCESS ' + data.orderId);
+      // webhook
+      const res = await axios.post(`${process.env.API_URL}/callback/scraper`, {
+        type: 'ORDER_COMPLETED',
+        data: data
+      })
+    } catch (error) {
+      console.log('RAZER PAYMENT - ERROR ' + data.orderId);
+      console.log(error);
+      const res = await axios.post(`${process.env.API_URL}/callback/scraper`, {
+        type: 'ORDER_FAILED',
+        data: { ...data, info: { error: error.message ?? 'scrap razer failed' } }
+      })
+    }
+    finally {
+      await page.close()
+
+    }
+
+    const endCounting = dayjs().unix()
+
+    console.log(`RAZER TOTAL SECONDS: ${endCounting - startCounting}`);
+    return true
+  })
+
+}
+
+
 
 const getTrxBCA = async (BCA_USERNAME, BCA_PASSWORD) => {
+  const startCounting = dayjs().unix()
 
   const username = cryptojs.AES.decrypt(BCA_USERNAME, process.env.SECRET_KEY).toString(cryptojs.enc.Utf8);
   const password = cryptojs.AES.decrypt(BCA_PASSWORD, process.env.SECRET_KEY).toString(cryptojs.enc.Utf8);
 
-  const scraper = new bankScraper.ScrapBCA(username, password, {
-    headless: process.env.APP_ENV == 'local' ? false : true,
-    executablePath: process.env.APP_ENV == 'local' && '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-  });
+  const scraper = new bankScraper.ScrapBCA(username, password);
   // todays date
   const date = dayjs().add(7, 'hour').format('D')
   const month = dayjs().add(7, 'hour').format('M')
 
   console.log(`Run at ${dayjs().format('DD/MM/YY, HH:mm')} - params: date ${date}, month ${month}`);
   try {
-    var res = await scraper.getSettlement(date, month, date, month)
+    var res = await scraper.getStatement(date, month, date, month)
   } catch (error) {
     throw new Error("failed to get data");
   }
 
-  const mapped = res.map(i => ({
+  const mapped = res.mutasi?.map(i => ({
     name: i.keterangan,
     type: i.mutasi,
     date: i.tanggal,
@@ -31,8 +178,21 @@ const getTrxBCA = async (BCA_USERNAME, BCA_PASSWORD) => {
     balance: parseInt(i.saldoakhir.substring(0, i.saldoakhir.length - 3).replaceAll(',', '')),
   }))
 
+  const endCounting = dayjs().unix()
+
+  console.log(`BCA TOTAL SECONDS: ${endCounting - startCounting}`);
 
   return mapped
+
 }
 
-module.exports = { getTrxBCA }
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+
+
+module.exports = { getTrxBCA, razerCheckoutPage, testScraper }
